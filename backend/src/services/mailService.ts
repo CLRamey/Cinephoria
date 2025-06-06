a0e9b5507fdad3afc4bbd0c8a767241444737a46
CLR <177638971+CLRamey@users.noreply.github.com>
2025-06-06 14:03:15 +0200

    Backend mail service

---
+++ backend/src/services/mailService.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT || '587', 10),
+  secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export const sendEmail = async (to: string, subject: string, text: string) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Cinephoria" <${process.env.SMTP_USER}>`, // sender address
+      to, // list of receivers
+      subject, // Subject line
+      text, // plain text body
+    });
+
+    console.log(`Message sent:, ${info.messageId}`);
+    return info;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
+};
