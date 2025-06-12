52369092ef7b8e1ba283324e96f59f411de5be50
CLR <177638971+CLRamey@users.noreply.github.com>
2025-06-12 12:01:36 +0200

    Update to the mailService

--- backend/src/services/mailService.ts
+++ backend/src/services/mailService.ts
@@ -2,23 +2,23 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: parseInt(process.env.SMTP_PORT || '587', 10),
-  secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
-
 export const sendEmail = async (to: string, subject: string, text: string) => {
   try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: parseInt(process.env.SMTP_PORT || '587', 10),
+      secure: process.env.SMTP_SECURE === 'true',
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
     const info = await transporter.sendMail({
-      from: `"Cinephoria" <${process.env.SMTP_USER}>`, // sender address
-      to, // list of receivers
-      subject, // Subject line
-      text, // plain text body
+      from: `"Cinephoria" <${process.env.SMTP_USER}>`,
+      to,
+      subject,
+      text,
     });
 
     console.log(`Message sent:, ${info.messageId}`);
