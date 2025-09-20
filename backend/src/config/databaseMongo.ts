8348c9ebc1a9f61273e9b1c237ae5a5ee3eac910
CLR <177638971+CLRamey@users.noreply.github.com>
2025-09-20 12:40:04 +0200

    Security improvements & production preparation

--- backend/src/config/databaseMongo.ts
+++ backend/src/config/databaseMongo.ts
@@ -4,14 +4,22 @@ import mongoose from 'mongoose';
 import { log, logerror } from '../utils/logger';
 
 const mongoURI = process.env['MONGO_URI'] as string;
+// Temporary disable SSL until this is obtained
+const useSSL = process.env['DB_SSL'] === 'true';
 
 // Connection to MongoDB
 export const connectMongo = async () => {
   try {
-    await mongoose.connect(mongoURI, {
+    const options: mongoose.ConnectOptions = {
       serverSelectionTimeoutMS: 60000, // Set a timeout for server selection
       socketTimeoutMS: 60000, // Set a timeout for socket operations
-    });
+    };
+    if (useSSL) {
+      options.tlsCAFile = process.env['MONGO_CA'];
+      options.tlsCertificateKeyFile = process.env['MONGO_CLIENT_COMBINED'];
+      options.tlsAllowInvalidCertificates = false;
+    }
+    await mongoose.connect(mongoURI, options);
     log('MongoDB connected successfully');
   } catch (error) {
     logerror('MongoDB connection error:', error);
--- projects/cinephoria-web/src/environments/environment.ts
+++ projects/cinephoria-web/src/environments/environment.ts
@@ -4,3 +4,11 @@ export const environment = {
   frontendDomain: 'http://localhost:8081',
   enableConsole: false, //Disable console logs in production
 };
+
+// PRE-DEPLOY SETUP:
+// apiURL: 'http://localhost:3001/api',
+// frontendDomain: 'http://localhost:8081',
+
+// PRODUCTION DOMAIN EXAMPLE: NOTE: Replace domain.dev with your domain
+// apiURL: 'https://domain.dev/api',
+// frontendDomain: 'https://domain.dev',
